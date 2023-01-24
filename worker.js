a38cbee6f5973e70b5420dcd0769d9366fc20727
Jon Allured <jon.allured@gmail.com>
2023-01-24 10:06:30 -0600

    Wire up a command to fill the form

--- worker.js
+++ worker.js
@@ -4,3 +4,13 @@ chrome.action.onClicked.addListener((tab) => {
     target: { tabId: tab.id },
   });
 });
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command !== "fillForm") return
+
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
+  chrome.scripting.executeScript({
+    files: ["scripts/fillForm.js"],
+    target: { tabId: tab.id },
+  });
+});
